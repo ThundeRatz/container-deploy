ef45868a80f704dc377d37d9c95e9ae388fc0b6b
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-11-05 13:55:59 -0200

    Start as ROS user, auto source ROS setup.bash

--- trekking/build.sh
+++ trekking/build.sh
@@ -3,4 +3,5 @@ set -e
 
 cd "$(dirname "$0")"
 docker build -t thunderatz/trekking .
-echo 'Run "docker run --rm -v <path to Trekking directory>:/home/ros/ThunderTrekking -ti thunderatz/trekking bash" to start a shell in the container'
+echo 'To start a shell in the container and allow USB access, run:'
+echo 'nvidia-docker run --rm -v <path to Trekking directory>:/home/ros/ThunderTrekking --privileged -v /dev/bus/usb:/dev/bus/usb -ti thunderatz/trekking bash'
